9969aac1d3de600312d75f4eeb68523a1dbcf38d
Marty Zhang <marty8zhang@gmail.com>
2020-06-15 12:51:55 +1000

    Tidy up `1_simplest-examples.js`.

--- promises/1_simplest-example.js
+++ promises/1_simplest-example.js
@@ -5,21 +5,20 @@
  * `resolve` and `reject` are acting like wrapper methods, which resolve/reject (wrap) the given result (provided as
  * their only parameter) to a predefined interface.
  */
-const myPromise = new Promise(function (resolve, reject) {
-  // Some operation.
-  const operationOutcome = Math.random()
+const myPromise = new Promise((resolve, reject) => {
+  const someOperationOutcome = Math.random()
 
-  if (operationOutcome >= 0.5) {
-    resolve('`myPromise` worked!')
+  if (someOperationOutcome >= 0.5) {
+    resolve('`myPromise` worked.')
   } else {
     reject(Error('`myPromise` broke!'))
   }
 })
 
-const optionalSuccessCallback = (result) => console.log(result)
-const optionalFailureCallback = (result) => console.log(result)
+const optionalOnfulfilled = (result) => console.log(result)
+const optionalOnrejected = (result) => console.log(`${result.name}: ${result.message}`)
 
 // Use the `Promise`.
-myPromise.then(optionalSuccessCallback, optionalFailureCallback)
+myPromise.then(optionalOnfulfilled, optionalOnrejected)
 
 console.log('`myPromise` has not started execution yet.')
